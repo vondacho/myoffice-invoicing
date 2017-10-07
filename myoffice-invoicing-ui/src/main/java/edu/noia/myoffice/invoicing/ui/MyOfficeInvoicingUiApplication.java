f9e65fd73f23e4627470c6b239fac39679f24721
vondacho <olivier.vondach@obya.ch>
2017-10-07 17:10:20 +0200

    initial commit

---
+++ myoffice-invoicing-ui/src/main/java/edu/noia/myoffice/invoicing/ui/MyOfficeInvoicingUiApplication.java
@@ -0,0 +1,12 @@
+package edu.noia.myoffice.invoicing.ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyOfficeInvoicingUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyOfficeInvoicingUiApplication.class, args);
+	}
+}
